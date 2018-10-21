92b05f9a9a4d9e55e80b5f6d48876b84d0c7fb3f
jhartma4 <43069303+jhartma4@users.noreply.github.com>
2018-10-21 12:13:53 -0400

    Updated index file
    Added point to footer file
    Removed footer code

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
   include 'nav.php';
   include 'header.php';
+  include 'footer.php';
 ?>
 
 <!DOCTYPE html>
@@ -60,18 +61,6 @@
 <div class="w3-container w3-black w3-center w3-opacity w3-padding-64">
     <h1 class="w3-margin w3-xlarge">Quote of the day: An apple a day, keeps the doctor away.</h1>
 </div>
-
-<!-- Footer -->
-<footer class="w3-container w3-padding-64 w3-center w3-opacity">  
-  <div class="w3-xlarge w3-padding-32">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
- </div>
- <p>Some footer text.</p>
-</footer>
 
 <script>
 // Used to toggle the menu on small screens when clicking on the menu button
