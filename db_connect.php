0637a4e5bfd732d9eb96015427ae3de5ac3c672a
jhartma4 <43069303+jhartma4@users.noreply.github.com>
2018-10-21 11:48:44 -0400

    Created database connection file

---
+++ db_connect.php
@@ -0,0 +1,8 @@
+<? php
+    $dbServer = "capstone";
+    $dbUsername = "root";
+    $dbPassword = "root";
+    $dbName = "blog_cms";
+
+    $conn = mysqli_connect($dbServer, $dbUsername, $dbPassword, $dbName);
+?>
