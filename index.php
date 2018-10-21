62f4753c3b65218bb75bc6cdb888f786294999f5
judy_hartman <jhartma4@lesley.edu>
2018-10-21 17:12:35 -0400

    Content changes

--- index.php
+++ index.php
@@ -59,12 +59,10 @@
     </div>
 
     <div class="w3-twothird">
-      <h1>Lorem Ipsum</h1>
-      <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
+      <h1>About Our Recipes</h1>
+      <h5 class="w3-padding-32">Written for cooks, by cooks</h5>
 
-      <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat.</p>
+      <p class="w3-text-grey">We know the struggle of figuring out what's for dinner and how to please everyone you're cooking for. Whether it's a regular weekday or special occasion, we have a solution. Our recipes are a collection or tried-and-true favorites, sure to please your entire family.</p>
     </div>
   </div>
 </div>
