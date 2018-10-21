53efd9d16b29c867cbeba2f01825d4b7cdf262e0
judy_hartman <jhartma4@lesley.edu>
2018-10-21 18:33:59 -0400

    Content edits

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <!-- Header -->
 <header class="w3-container w3-red w3-center" style="padding:128px 16px">
-  <h1 class="w3-margin w3-jumbo">Favorite Recipes</h1>
-  <p class="w3-xlarge">Recipes for everyone!</p>
+  <h1 class="w3-margin w3-jumbo">Recipes that Rock!</h1>
+  <p class="w3-xlarge">Written for cooks, by cooks</p>
   <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Add a Recipe</button>
 </header>
