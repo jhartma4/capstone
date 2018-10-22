9f4a2df3a1c33ead4d76af638092425f1bcc77d6
JT <jt@pxl.la>
2018-10-22 10:48:11 -0700

    Created pages for appetizers and drinks

--- appetizers.php
+++ appetizers.php
@@ -16,17 +16,16 @@
   </head>
 <body>
 
-<header class="w3-container w3-red w3-center" style="padding:128px 16px">
-  <h1 class="w3-margin w3-jumbo">Recipes for Entertaining</h1>
-  <p class="w3-xlarge">Easy recipes to make your party a hit!</p>
-  <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Submit a recipe!</button>
+<header class="w3-container w3-blue w3-center" style="padding:60px 16px">
+  <h1 class="w3-margin w3-jumbo">Appetizers</h1>
+  <p class="w3-xlarge">Everyone loves these easy to make favorites.</p>
 </header>
 
 <!-- First Grid -->
 <div class="w3-row-padding w3-padding-64 w3-container">
   <div class="w3-content">
     <div class="w3-twothird">
-      <h1>Appetizers everyone will love!</h1>
+      <h1>Appetizers</h1>
       <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
 
       <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
@@ -50,7 +49,7 @@
     </div>
 
     <div class="w3-twothird">
-      <h1>Festive drinks!</h1>
+      <h1>Easy to make!</h1>
       <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
 
       <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
--- drinks.php
+++ drinks.php
@@ -1,5 +1,6 @@
 <?php
   include 'nav.php';
+  include 'header.php';
 ?>
 
 <!DOCTYPE html>
@@ -16,17 +17,16 @@
   </head>
 <body>
 
-<header class="w3-container w3-red w3-center" style="padding:128px 16px">
-  <h1 class="w3-margin w3-jumbo">Recipes for Entertaining</h1>
-  <p class="w3-xlarge">Easy recipes to make your party a hit!</p>
-  <button class="w3-button w3-black w3-padding-large w3-large w3-margin-top">Submit a recipe!</button>
+<header class="w3-container w3-green w3-center" style="padding:60px 16px">
+  <h1 class="w3-margin w3-jumbo">Cocktails</h1>
+  <p class="w3-xlarge">Get the party started with these fun libations.</p>
 </header>
 
 <!-- First Grid -->
 <div class="w3-row-padding w3-padding-64 w3-container">
   <div class="w3-content">
     <div class="w3-twothird">
-      <h1>Appetizers everyone will love!</h1>
+      <h1>Cocktails everyone will love!</h1>
       <h5 class="w3-padding-32">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</h5>
 
       <p class="w3-text-grey">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Excepteur sint
